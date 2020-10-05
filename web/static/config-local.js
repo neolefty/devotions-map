2ee0554f9ca5681f2ae0ed01e5af72bc0330be3b
Bill Baker <bill@beechersoftware.com>
2020-10-04 21:06:26 -0400

    new map style

--- web/static/config-local.js
+++ web/static/config-local.js
@@ -1,5 +1,6 @@
 window.config = {
     // permissive token — do not commit to git
     mapboxToken: 'pk.eyJ1IjoibmVvbGVmdHkiLCJhIjoiY2tjaTdpZHBnMG9tdjJ5bGljNWJuemx3NSJ9.JoUUnG5ab677stL30sF5Hg',
-    mapboxStyleUrl: 'mapbox://styles/neolefty/ckfopcinm05ps19pf4zaszzub/draft',
+    mapboxStyleUrl: 'mapbox://styles/neolefty/ckfvridqx1tl619obkj1dnkli/draft',
+    // mapboxStyleUrl: 'mapbox://styles/neolefty/ckfopcinm05ps19pf4zaszzub/draft',
 }
