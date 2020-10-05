2e82c9b28515c1e0359b3bb929dabcb691bb37e2
Bill Baker <bill@beechersoftware.com>
2020-10-04 21:14:07 -0400

    missing comma

--- web/static/config-prod.js
+++ web/static/config-prod.js
@@ -1,6 +1,6 @@
 window.config = {
     // locked to data.midwestbahai.org
     mapboxToken: 'pk.eyJ1IjoibmVvbGVmdHkiLCJhIjoiY2tjaThiczlxMHZjNTJwcGE0MmdlNGw2cCJ9.tddrlBIw_-8TFsNO-FlNyA',
-    mapboxStyleUrl: 'mapbox://styles/neolefty/ckfvridqx1tl619obkj1dnkli,
+    mapboxStyleUrl: 'mapbox://styles/neolefty/ckfvridqx1tl619obkj1dnkli',
     // mapboxStyleUrl: 'mapbox://styles/neolefty/ckfopcinm05ps19pf4zaszzub',
 }
