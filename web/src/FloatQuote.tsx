5b681c93259bea6dd94d6264b60ba82277ba1737
Bill Baker <bill@beechersoftware.com>
2020-10-13 23:50:38 -0400

    add quote

---
+++ web/src/FloatQuote.tsx
@@ -0,0 +1,26 @@
+import {createStyles, makeStyles} from "@material-ui/styles"
+import React, {PropsWithChildren} from "react"
+
+const useStyles = makeStyles(createStyles({
+    float: {
+        color: 'rgb(244, 224, 40)',
+        background: 'rgba(30, 40, 120, 0.4)',
+        border: '0.3vmax solid rgba(45, 60, 180, 0.5)',
+        position: 'fixed',
+        textAlign: 'center',
+        bottom: '1.5vmax',
+        left: '1.5vmax',
+        width: '30vmax',
+        fontSize: '2vmax',
+        padding: '0.5vmax',
+        fontFamily: 'georgia',
+        fontStyle: 'italic',
+    },
+}))
+
+export const FloatQuote = (props: PropsWithChildren<{}>) => {
+    const classes = useStyles()
+    return (
+        <div className={classes.float}>{props.children}</div>
+    )
+}
